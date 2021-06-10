7f9d60b56758e5c91092762af6216d3d98a5c848
jonny827 <jonny827@users.noreply.github.com>
2021-06-10 03:29:55 -0700

    Add files via upload
    Initial WordPress plugin commit.

---
+++ content-coin-mint.php
@@ -0,0 +1,25 @@
+<?
+/**
+ * Plugin Name
+ *
+ * @package           Content_Coin_Mint
+ * @author            Jonathan Allcock
+ * @copyright         2020 Content Coin
+ * @license           GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Content Coin Mint
+ * Plugin URI:        https://contentcoin.com/wordpress/plugins/content-coin-mint
+ * Description:       This plugin integrates with the Content Coin Network to mint coins on various NFT enabled blockchains.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Jonathan Allcock
+ * Author URI:        https://contentcoin.com
+ * Text Domain:       plugin-slug
+ * License:           GPL v3 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path:       /languages
+ */
+ 
+ 
